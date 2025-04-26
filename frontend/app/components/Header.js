3a64ff32b6332d90a7711ae35284856bdaf408c8
Betty Woldemichael <bettget@gmail.com>
2025-04-26 14:48:04 -0400

    update hero image and navigation bar

--- frontend/app/components/Header.js
+++ frontend/app/components/Header.js
@@ -1,18 +1,24 @@
 // app/components/Header.js
 import styles from '../styles/Home.module.css';
-import Link from 'next/link'
+import Link from 'next/link';
+import Image from 'next/image';
+
 
 export default function Header() {
   return (
     <header className={styles.header}>
-      <div className={styles.logo}>Group 2 Logo</div>
+      <div className={styles.logo}>
+      <Link href="/">
+          <Image src="/logo.png" alt="Work Wing Logo" width={180} height={60} />
+        </Link>
+      </div>
       <nav className={styles.nav}>
-        <a href="#">Job Listings</a>
+        <a href="#">About Us</a>
         <a href="#">My Profile</a>
         <a href="#">Resources</a>
       </nav>
       <Link href="/login" className={styles.signupBtn}>Log In</Link>
-      <button className={styles.signupBtn}>Sign Up</button>
+      {/* <button className={styles.signupBtn}>Sign Up</button> */}
     </header>
   );
 }
--- frontend/app/components/Hero.js
+++ frontend/app/components/Hero.js
@@ -1,19 +1,17 @@
-// app/components/Hero.js
 import Image from 'next/image';
 import styles from '../styles/Home.module.css';
 
 export default function Hero() {
   return (
-    <section className={styles.hero}>
-      <div className={styles.heroText}>
+    <section className={styles.heroSection}>
+      <div className={styles.heroBackground}>
+        <Image src="/hero.png" alt="Job Search" fill style={{ objectFit: 'cover' }} />
+      </div>
+      <div className={styles.heroContent}>
         <h1>Revolutionize Your Job Search Experience Today</h1>
         <p>Smart tools to help you find, apply, and track jobs better.</p>
         <button className={styles.ctaButton}>Get Started</button>
       </div>
-      <div className={styles.heroImage}>
-        <Image src="/hero.png" alt="Job Search" fill style={{ objectFit: 'cover' }} />
-      </div>
     </section>
   );
 }
-
