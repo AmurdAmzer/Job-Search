b6957ef304ab760fe5361b7360320ec665f00c80
devereaweaver <devereaweaver@icloud.com>
2025-05-04 21:56:29 -0400

    Added code to pull jobs data to the frontend pages

--- frontend/app/signup/page.js
+++ frontend/app/signup/page.js
@@ -16,7 +16,7 @@ export default function SignUpPage() {
     setError('');
 
     try {
-      const res = await fetch('http://localhost:5000/api/users/register', {
+      const res = await fetch('http://localhost:9999/api/users/register', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(form),
