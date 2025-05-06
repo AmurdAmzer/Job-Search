ea94aa0424f194fb3df418412bc914da37ead758
devereaweaver <devereaweaver@icloud.com>
2025-05-05 21:09:02 -0400

    removed everything from main to readd the two branches, need to do so for deployment

--- backend/backend/src/routes/favorites.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { saveFavorite, getFavorites, deleteFavorite } = require('../controllers/favoritesController');
-const router = express.Router();
-
-router.post('/', saveFavorite);
-router.get('/:userId', getFavorites);
-router.delete('/:userId/:sourceId', deleteFavorite);
-
-module.exports = router;
